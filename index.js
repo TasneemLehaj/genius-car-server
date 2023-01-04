4a24c9e2a7456437b8eacb65d874d43932cf251b
TasneemLehaj <tasneemlehaj20@gmail.com>
2023-01-05 03:50:23 +0600

    basic node mongo setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);... egula pore soriye felte hobe
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.iyieycw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('genius car server is running');
+})
+
+app.listen(port, () => {
+    console.log(`genius car server is running on port: ${port}`)
+})
